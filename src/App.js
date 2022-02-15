fecf9e1a94af7ffe7147d3c911958c2900c456b1
DOBBI <cocomong_94@naver.com>
2022-02-15 23:14:27 +0900

    2022-02-15

--- src/App.js
+++ src/App.js
@@ -1,28 +1,76 @@
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-
-  
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((response) => response.json())
-    .then((json) => {
-      setCoins(json) // json => 코인들, 배열에 담아줌
-      setLoading(false)
-    })
-  }, []) // 빈 배열이면 이 코드는 한 번만 작동한다 !
+  // return null; // 페이지에서 아무것도 보지 않는다는 것을 의미 !
   return (
-    <div>
-      <h1> 💰BITCOIN💰 {loading ? "" : `Total : ${coins.length}`}</h1>
-      {loading ? <strong>Loading...</strong> : 
-        <select>
-      {coins.map((coin) => <option>
-      {coin.name}({coin.symbol}): {coin.quotes.USD.price} USD</option>)}
-    </select>} {/* <strong> 요소는 콘텐츠 자체의 중요성을 강조할 때 사용 */}
-    {/* 여기서 coin은 coins array안에 있는 각각의 coin을 의미 */}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
+
 export default App;
+
+// router는 URL을 보고있는 component
+
+//! 비트코인 사이트
+// function App() {
+//   const [loading, setLoading] = useState(true)
+//   const [coins, setCoins] = useState([])
+//   const [coinValue, setCoinValue] = useState(0)
+//   const [money, setMoney] = useState(0)
+
+//   useEffect(() => {
+//     fetch("https://api.coinpaprika.com/v1/tickers")
+//       .then((res) => res.json())
+//       .then((json) => {
+//         setCoins(json)
+//         setLoading(false)
+//       })
+//   }, [])
+
+//   const onChange = (e) => {
+//     setMoney(e.target.value)
+//   }
+
+//   const coinChange = (e) => {
+//     setCoinValue(e.target.value)
+//   }
+
+//   return (
+//     <div>
+//       <h1>💰BITCOIN💰 {loading ? "" : `Total : ${coins.length}`}</h1>
+
+//       {loading ? <strong>Loading...</strong> :
+//       <select onChange={coinChange}>
+//         <option>----- 😈 Select Coin 😈 -----</option>
+//         {coins.map((coin, index) => (
+//           <option key={index} value={coin.quotes.USD.price}>
+//             {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+//           </option>
+//         ))}
+//       </select>
+//       }
+//       <hr />
+
+//       <div>
+//         <input type="number" placeholder="How much you have?" onChange={onChange}/>
+//       </div>
+
+//       <h1>
+//         {(money !== 0 && coinValue !== 0) ? (money / coinValue) : null}
+//       </h1>
+
+//     </div>
+//   )
+// }
+//export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,32 @@
+import Movie from "../components/Movie";
+import { useEffect, useState } from "react";
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie key={movie.id} coverImg={movie.medium_cover_image} title={movie.title} summary={movie.summary} genres={movie.genres} />
+          ))}
+          {/* key는 React.js에서만, map안에서 component들을 render할 때 사용 ! */}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
