7ea0c9b942db4f78d32bfb94f51a5e12ca52c101
DOBBI <cocomong_94@naver.com>
2022-02-16 20:51:25 +0900

    2022-02-16

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,15 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-function Movie({ coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, summary, genres }) {
+  //! props 칸에 들어간 이것들 전부 Object인 것을 잊지말자 !
   // props받아오기 !
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         <li>
@@ -19,6 +23,7 @@ function Movie({ coverImg, title, summary, genres }) {
 }
 
 Movie.prototype = {
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -20,7 +20,7 @@ function Home() {
       ) : (
         <div>
           {movies.map((movie) => (
-            <Movie key={movie.id} coverImg={movie.medium_cover_image} title={movie.title} summary={movie.summary} genres={movie.genres} />
+            <Movie key={movie.id} id={movie.id} coverImg={movie.medium_cover_image} title={movie.title} summary={movie.summary} genres={movie.genres} />
           ))}
           {/* key는 React.js에서만, map안에서 component들을 render할 때 사용 ! */}
         </div>
